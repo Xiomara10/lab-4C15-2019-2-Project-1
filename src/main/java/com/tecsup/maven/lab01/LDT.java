753738ff4939fe6ee87e287d2ddd9f880f00cd6b
fernann234 <43591517+fernann234@users.noreply.github.com>
2019-08-27 17:29:54 -0500

    nueva clase

---
+++ src/main/java/com/tecsup/maven/lab01/LDT.java
@@ -0,0 +1,13 @@
+package com.tecsup.maven.lab01;
+
+/**
+ * Hello world!
+ *
+ */
+public class LDT 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
